1eaab7541093b2e9f2bec49a4b85c43ab1379a13
Robert Hall <langbert@gmail.com>
2020-02-07 21:04:53 -0700

    Come back to…

--- svelte/modals/NameLayout.svelte
+++ svelte/modals/NameLayout.svelte
@@ -6,10 +6,10 @@
 			<h1>{modal.name}</h1>
 			<!-- <p>Modal: {JSON.stringify(modal)}</p> -->
 			{#each modal.rootclip.sequences as sequence}
-				<div class="sequence">
+				<div class="sequence {sequence.classes ? sequence.classes : ''}">
 					<h2>{sequence.name}</h2>
 					{#each sequence.clips as clip}
-						<div class="clip">
+						<div class="clip {clip.classes ? clip.classes : ''}">
 							<h3>{clip.name}</h3>
 							<div class="detail">
 								{@html clip.html}
@@ -37,4 +37,58 @@
 	// $: html = modal.html ? modal.html.replace(/<p><\/p>/gi, '') : ''
 </script>
 
-<!-- <style type="text/scss"></style> -->
+<style type="text/scss">
+	.sequence {
+		padding: 50rem;
+		border: 3px solid black;
+	}
+	.clip {
+		margin: 0 0 50rem;
+		padding: 50rem;
+		background-color: white;
+	}
+	.detail {
+		& :global {
+			> p:first-child::first-letter {
+				float: left;
+				margin: -6rem 0 0 0;
+				padding: 0 12rem 6rem 0;
+				// background-color: red;
+				font-size: 100rem;
+				line-height: 80rem;
+			}
+			.media {
+				width: 100%;
+				margin: 50rem 0;
+				line-height: 0;
+			}
+			img,
+			video {
+				margin: 0;
+				width: 100%;
+			}
+			.media + .source,
+			.media + .caption {
+				margin: -44rem 0 50rem;
+			}
+			.media + .source + .caption {
+				margin: -48rem 0 50rem;
+			}
+			.source {
+				color: #b2b2b2;
+				font: $light 13rem/18rem $font;
+			}
+			.caption {
+				font: $bold 13rem/18rem $font;
+				text-transform: uppercase;
+			}
+			.media,
+			.source,
+			.caption {
+				p {
+					margin: 0;
+				}
+			}
+		}
+	}
+</style>
