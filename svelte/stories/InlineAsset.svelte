9e488b2bf0f4b2725c8681c2de6707b78d055bfe
Robert Hall <langbert@gmail.com>
2020-03-21 20:20:25 -0600

    Inline asset

---
+++ svelte/stories/InlineAsset.svelte
@@ -0,0 +1,49 @@
+{#if asset}
+	<div class="asset">
+		{#if type === 'video'}
+			<InlineVideo {asset} {options}/>
+		{:else if type === 'audio'}
+			InlineAudio asset options
+		{:else}
+			<InlineImage {asset} {options}/>
+		{/if}
+	</div>
+{/if}
+
+<script>
+	export let assets = []
+	export let options = {}
+
+	import InlineVideo from './InlineVideo.svelte'
+	// import InlineAudio from './InlineAudio.svelte'
+	import InlineImage from './InlineImage.svelte'
+
+	$: videos = assets.filter(asset => asset.mime_type.includes('video'))
+	$: audios = assets.filter(asset => asset.mime_type.includes('audio'))
+	$: images = assets.filter(asset => asset.mime_type.includes('image'))
+	$: asset = videos.length ? videos[0] : (
+		audios.length ? audios[0] : (
+			images.length ? images[0] : undefined
+		)
+	)
+	$: type = asset ? asset.mime_type.split('/')[0] : undefined
+
+	$: asset_two
+		= (type === 'video' || type === 'audio') && images.length
+		? images[0]
+		: undefined
+	// $: type_two = asset_two ? asset_two.mime_type.split('/')[0] : undefined
+</script>
+
+<style type="text/scss">
+	.asset {
+		overflow: hidden;
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 1;
+		background-color: pink;
+	}
+</style>
---
+++ svelte/stories/InlineImage.svelte
@@ -0,0 +1,76 @@
+<div class:show class="bg-wrap">
+	<img {src} alt="n/a" use:lazy/>
+	<div class:show class="bg" style="background-image:url({src});"></div>
+</div>
+
+<script>
+	export let asset
+	export let options
+
+	let loaded = false
+	let show = false
+	let src; $: {
+		let path = asset.url.split(asset.handle)[0]
+		if (options.width || options.height) {
+			path += 'resize='
+			if (options.width && options.height) {
+				path += `w:${options.width},h:${options.height}`
+			} else if (options.width) {
+				path += `w:${options.width}`
+			} else {
+				path += `h:${options.height}`
+			}
+			path += options.crop ? ',fit:crop/' : '/'
+		} else if (options.crop) {
+			// just a default size if crop only
+			path += 'resize=w:600,h:600,fit:crop/'
+		}
+		src = path + asset.handle
+	}
+
+	function lazy(node) {
+		if (loaded) {
+			node.setAttribute('src', src)
+		} else {
+			const img = new Image()
+			img.onload = () => {
+				loaded = true
+				node.setAttribute('src', src)
+				setTimeout(() => show = true, 100)
+			}
+			img.src = src
+		}
+		return { destroy() {} } // noop
+	}
+</script>
+
+<style type="text/scss">
+	.bg-wrap {
+		overflow: hidden;
+		position: relative;
+		width: 100%;
+		height: 100%;
+		background-color: #f7f7f7;
+	}
+	.bg {
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		background: none no-repeat center transparent;
+		background-size: cover;
+		opacity: 0;
+		transition: opacity 0.5s ease-in-out;
+		&.show {
+			opacity: 1;
+		}
+	}
+	img {
+		opacity: 0;
+		position: absolute;
+		top: 0;
+		left: 0;
+		pointer-events: none;
+	}
+</style>
