769fc04f90501e46937d86484aad29ecc94877f9
Robert Hall <langbert@gmail.com>
2020-05-20 21:42:50 -0600

    Slight clean up

--- api/stories/single.post.js
+++ api/stories/single.post.js
@@ -2,8 +2,7 @@ export default async function(req, res) {
 	try {
 		const path = `${process.env.LYNX_API_PLUGIN}/api/stories/single.post.js`
 		const { default: api } = await import(path)
-		const { story } = await api({ title: req.body.title })
-		return res.json(story)
+		return res.json(await api(req.body))
 	} catch (error) {
 		// console.log(error)
 		return res.json({ error: 1, message: error.message })
