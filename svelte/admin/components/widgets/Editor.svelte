a8d46368ef90924ae3974f84a1ccd1e83c9d5c31
Robert Hall <langbert@gmail.com>
2020-05-21 00:14:42 -0600

    Freaking awesome editor

---
+++ svelte/admin/components/widgets/Editor.svelte
@@ -0,0 +1,123 @@
+<button on:click={show}>Show HTML</button>
+
+<div id="editor-js" class="editor-js"></div>
+
+<script>
+	let editor
+	let components = []
+
+	import SimpleImageComponent from './simple-image/SimpleImageComponent.svelte'
+
+	import { onMount, onDestroy } from 'svelte'
+	onMount(async() => {
+		const EditorJS = (await import('@editorjs/editorjs')).default
+		const Header = (await import('@editorjs/header')).default
+		const List = (await import('@editorjs/list')).default
+		const { ConstructSimpleImage } = await import('./simple-image/SimpleImage.js')
+		const SimpleImage = ConstructSimpleImage(SimpleImageComponent, components)
+
+		window.editor = editor = new EditorJS({
+			data: {
+				time: 1590045282866,
+				blocks: [{
+					type: 'header',
+					data: {
+						text: 'Hello World',
+						level: 2
+					}
+				}, {
+					type: 'paragraph',
+					data: { text: 'this is a whole thing' }
+				}, {
+					type: 'list',
+					data: {
+						style: 'ordered',
+						items: ['asdfasf', 'dsa', 'fdsa', 'adfs', 'fdasfsdg']
+					}
+				}, {
+					type: 'paragraph',
+					data: { text: 'gfd' }
+				}, {
+					type: 'image',
+					data: {
+						url: 'https://media.graphcms.com/resize=w:650,h:500,fit:crop/FucZTm79RPyWd3RLry6L',
+					},
+				}],
+				version: '2.17.0'
+			},
+			holderId: 'editor-js',
+			tools: {
+				// https://github.com/editor-js/header
+				header: {
+					class: Header,
+					config: {
+						placeholder: 'Enter a header',
+						levels: [1, 2, 3],
+						defaultLevel: 2,
+					},
+				},
+
+				// https://github.com/editor-js/list
+				list: List,
+				image: {
+					class: SimpleImage,
+				},
+			},
+			autofocus: true,
+			logLevel: 'ERROR',
+
+		})
+	})
+
+	async function show() {
+		const data = await editor.save()
+	}
+
+	// // import { saveable, hot, hot_loaded, hot_changes } from '../../../../stores/admin-store.js'
+	// import { onMount, onDestroy } from 'svelte'
+
+	onDestroy(() => {
+		// if ($hot) {
+		// 	$hot.destroy()
+		// 	$hot = null
+		// }
+		if ((editor && editor.destroy) || components.length) {
+			for (let comp of components) {
+				comp.$destroy()
+			}
+			editor.destroy()
+		}
+	})
+</script>
+
+<style type="text/scss">
+	.editor-js {
+		max-width: 850rem;
+		padding: 100rem;
+		border: 1rem solid var(--admin-gray-light);
+		box-shadow: 0rem 2rem 8rem rgba(0, 0, 0, 0.15);
+		background-color: #fafafa;
+		:global {
+			.ce-block {
+				padding-left: 8rem;
+				padding-right: 8rem;
+				background-color: white;
+				border: 1rem solid var(--admin-gray-light);
+				border-bottom: 0;
+				&:last-child {
+					border-bottom: 1rem solid var(--admin-gray-light);
+				}
+			}
+			svg {
+				// flex: 1;
+				// vertical-align: middle;
+				height: auto;
+				width: auto;
+			}
+			// h1, h2, h3, h4 { margin: 0; }
+			.codex-editor__redactor {
+				padding-bottom: 0 !important;
+			}
+		}
+	}
+</style>
---
+++ svelte/admin/components/widgets/simple-image/SimpleImageComponent.svelte
@@ -0,0 +1,8 @@
+<div class="wrapper">
+	<input type="text" bind:value={src} placeholder="Paste an image URL..."/>
+	<img {src} alt="No alt."/>
+</div>
+
+<script>
+	export let src = ''
+</script>
